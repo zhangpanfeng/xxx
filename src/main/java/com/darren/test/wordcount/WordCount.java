7abb0d36502a1443a263ba243237b3b9ebec5897
Zhang <Darren.Zhang@nielsen.com>
2016-10-26 17:33:40 +0800

    test

--- src/main/java/com/darren/test/wordcount/WordCount.java
+++ src/main/java/com/darren/test/wordcount/WordCount.java
@@ -4,13 +4,13 @@ package com.darren.test.wordcount;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-import com.sun.jersey.core.impl.provider.entity.XMLJAXBElementProvider.Text;
 
 public class WordCount {
     public static void main(String[] args) throws Exception {
